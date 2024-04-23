75127f0b0cc028a4fc934365cdc6290e96c3486c
gatekeep06 <mvmetacontent@gmail.com>
2024-04-23 14:36:31 +0300

    remove registering from initialization class

--- src/main/java/com/metacontent/trainersbackpack/TrainersBackpack.java
+++ src/main/java/com/metacontent/trainersbackpack/TrainersBackpack.java
@@ -7,12 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TrainersBackpack implements ModInitializer {
-    public static final String ID = "trainers-backpack";
+    public static final String ID = "trainersbackpack";
     public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
     @Override
     public void onInitialize() {
-        TrainersBackpackBlocks.registerBlocks();
-        TrainersBackpackItems.registerItems();
     }
 }
