2135467ef263e6a235c4e0f6055bf676f5fe3aa5
gatekeep06 <mvmetacontent@gmail.com>
2024-04-23 09:38:10 +0300

    adding backpack names to reference list

--- src/main/java/com/metacontent/trainersbackpack/item/TrainersBackpackItems.java
+++ src/main/java/com/metacontent/trainersbackpack/item/TrainersBackpackItems.java
@@ -6,10 +6,14 @@ import com.tiviacz.travelersbackpack.blocks.TravelersBackpackBlock;
 import com.tiviacz.travelersbackpack.init.ModItemGroups;
 import com.tiviacz.travelersbackpack.init.ModItems;
 import com.tiviacz.travelersbackpack.items.TravelersBackpackItem;
+import com.tiviacz.travelersbackpack.util.Reference;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrainersBackpackItems {
     public static final TravelersBackpackItem HEALING_BACKPACK_ITEM = registerBackpackItem("healing_backpack", TrainersBackpackBlocks.HEALING_BACKPACK_BLOCK);
@@ -20,7 +24,13 @@ public class TrainersBackpackItems {
 
     public static void registerItems() {
         TrainersBackpack.LOGGER.info("Registering items for " + TrainersBackpack.ID);
+
         ModItems.BACKPACKS.add(HEALING_BACKPACK_ITEM);
+
+        List<String> backpackNames = new ArrayList<>(List.of(Reference.BACKPACK_NAMES));
+        backpackNames.add("healing_backpack");
+        Reference.BACKPACK_NAMES = backpackNames.toArray(new String[]{});
+
         ItemGroupEvents.modifyEntriesEvent(ModItemGroups.TRAVELERS_BACKPACK).register(entries -> {
             entries.add(HEALING_BACKPACK_ITEM);
         });
