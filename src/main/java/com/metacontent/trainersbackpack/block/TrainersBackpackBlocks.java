e953782ebdb67e1e111a951b8e133821dce4cbed
gatekeep06 <mvmetacontent@gmail.com>
2024-04-23 14:37:56 +0300

    change block settings

--- src/main/java/com/metacontent/trainersbackpack/block/TrainersBackpackBlocks.java
+++ src/main/java/com/metacontent/trainersbackpack/block/TrainersBackpackBlocks.java
@@ -3,13 +3,14 @@ package com.metacontent.trainersbackpack.block;
 import com.metacontent.trainersbackpack.TrainersBackpack;
 import com.tiviacz.travelersbackpack.blocks.TravelersBackpackBlock;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class TrainersBackpackBlocks {
-    public static final TravelersBackpackBlock HEALING_BACKPACK_BLOCK = registerBackpackBlock("healing_backpack", FabricBlockSettings.copyOf(Blocks.WHITE_WOOL));
+    public static final TravelersBackpackBlock HEALING_BACKPACK_BLOCK = registerBackpackBlock("healing_backpack", FabricBlockSettings.copyOf(FabricBlockSettings.create().mapColor(MapColor.IRON_GRAY).sounds(BlockSoundGroup.METAL)));
 
     private static TravelersBackpackBlock registerBackpackBlock(String name, FabricBlockSettings settings) {
         return Registry.register(Registries.BLOCK, new Identifier(TrainersBackpack.ID, name),
