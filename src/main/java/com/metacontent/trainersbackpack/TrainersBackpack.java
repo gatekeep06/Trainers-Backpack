8722185a5522669aff5401d5f347b5118d42a373
gatekeep06 <mvmetacontent@gmail.com>
2024-04-23 14:42:14 +0300

    add blockstate

--- src/main/java/com/metacontent/trainersbackpack/TrainersBackpack.java
+++ src/main/java/com/metacontent/trainersbackpack/TrainersBackpack.java
@@ -1,7 +1,5 @@
 package com.metacontent.trainersbackpack;
 
-import com.metacontent.trainersbackpack.block.TrainersBackpackBlocks;
-import com.metacontent.trainersbackpack.item.TrainersBackpackItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
